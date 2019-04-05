22f451185764c4cc22d02df23a658d81cfe8f35d
Shubham Jain <s.shubjain@gmail.com>
2019-04-05 16:08:00 +0530

    forgot https in uri

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const SERVER_URI = process.env.NODE_ENV === 'production' ? "fast-bastion-68939.herokuapp.com" : 
+const SERVER_URI = process.env.NODE_ENV === 'production' ? "https://fast-bastion-68939.herokuapp.com" : 
   'http://localhost:3001';
 const INSTANCE_LOCATER = 'v1:us1:e4044788-6361-43c9-ba0a-6b00c0045020'
 
