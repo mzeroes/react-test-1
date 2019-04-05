e090a8030ae40b5055b26ff2c5ddefd34b6b2996
Shubham Jain <s.shubjain@gmail.com>
2019-04-05 16:04:11 +0530

    Fixed Server URI for heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const SERVER_URI = 'http://localhost:3001'
+const SERVER_URI = process.env.NODE_ENV === 'production' ? "fast-bastion-68939.herokuapp.com" : 
+  'http://localhost:3001';
 const INSTANCE_LOCATER = 'v1:us1:e4044788-6361-43c9-ba0a-6b00c0045020'
 
 
